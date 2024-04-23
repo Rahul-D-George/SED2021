7e740b27a06348c9ebb8e54b23e58f581123108f
rg922 <rbc@imperial.ac.uk>
2024-04-23 13:34:12 +0100

    feat: implemented tennis scorer model unit tests

---
+++ Q2/test/tennis/TennisScorerModelTest.java
@@ -0,0 +1,48 @@
+package tennis;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class TennisScorerModelTest {
+
+    @Test
+    public void FourConsecutivePointsLeadsToGame() {
+        TennisScorerModel ts = new TennisScorerModel();
+        for (int i = 0; i < 4; i++) {
+            ts.playerOneWinsPoint();
+        }
+        assertThat(ts.score(), containsString("Game Player 1"));
+    }
+
+    @Test
+    public void FortyFortyLeadsToDeuceAdvantageThenWin() {
+        TennisScorerModel ts = new TennisScorerModel();
+        for (int i = 0; i < 3; i++) {
+            ts.playerOneWinsPoint();
+            ts.playerTwoWinsPoint();
+        }
+        assertThat(ts.score(), containsString("Deuce"));
+        ts.playerOneWinsPoint();
+        assertThat(ts.score(), containsString("Advantage Player 1"));
+        ts.playerOneWinsPoint();
+        assertThat(ts.score(), containsString("Game Player 1"));
+    }
+
+    @Test
+    public void LossWithAdvantageLeadsToDeuce() {
+        TennisScorerModel ts = new TennisScorerModel();
+        for (int i = 0; i < 3; i++) {
+            ts.playerOneWinsPoint();
+            ts.playerTwoWinsPoint();
+        }
+        assertThat(ts.score(), containsString("Deuce"));
+        ts.playerOneWinsPoint();
+        assertThat(ts.score(), containsString("Advantage Player 1"));
+        ts.playerTwoWinsPoint();
+        assertThat(ts.score(), containsString("Deuce"));
+    }
+
+}
